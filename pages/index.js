2dc18d81de4fa53cc2e2b856505e41123b2ca792
= <=>
2021-03-07 21:04:30 +0600

    Header & Nav pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,3 +13,4 @@ export default function Home() {
     </div>
   )
 }
+
