b1910e824b79f349f3c3e323aba166eca502e3cd
= <=>
2021-03-07 18:42:00 +0600

    Layout page added

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const About = () =>{
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+            <h3>This is about page</h3>
+        </div>
+    )
+}
+export default About
